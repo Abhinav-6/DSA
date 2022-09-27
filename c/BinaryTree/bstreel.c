ecec8727c5a5e8136cc184a0f2a456850e38033b
Abhinav Kumar <89967724+Abhinav-6@users.noreply.github.com>
2022-09-27 05:40:47 +0000

    Added function to check if binary tree is binary search tree

--- c/BinaryTree/bstreel.c
+++ c/BinaryTree/bstreel.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MIN_VAL -1000000
+#define MAX_VAL 1000000
 
 struct bstNode {
   int data;
@@ -12,9 +14,11 @@ void postOrderTraversal(struct bstNode *root);
 struct bstNode *getNewNode(int data);
 struct bstNode *insert(struct bstNode *root, int data);
 void Insert(struct bstNode **root, int data);
-int minEle(struct bstNode* root);
-int maxEle(struct bstNode* root);
-int isBsTree(struct bstNode* root);
+int minEle(struct bstNode *root);
+int maxEle(struct bstNode *root);
+int isBsTree(struct bstNode *root);
+int isbsTree(struct bstNode *root);
+int isbsTreeutil(struct bstNode *root, int, int);
 int main() {
   struct bstNode *root = NULL;
   root = insert(root, 100);
@@ -33,10 +37,15 @@ int main() {
   preOrderTraversal(root);
   printf("\n");
   postOrderTraversal(root);
-	printf("\n");
-	printf("min Ele = %d\n", minEle(root));
-	printf("max Ele = %d\n", maxEle(root));
-	isBsTree(root)?printf("Binary tree is Binay search tree"):printf("Binary Tree is not Binary Search Tree");
+  printf("\n");
+  printf("min Ele = %d\n", minEle(root));
+  printf("max Ele = %d\n", maxEle(root));
+  isBsTree(root) ? printf("Binary tree is Binay search tree")
+                 : printf("Binary Tree is not Binary Search Tree");
+
+	isbsTree(root) ? printf("Binary tree is Binay search tree")
+                 : printf("Binary Tree is not Binary Search Tree");
+	
 }
 
 void Print(struct bstNode *root) {
@@ -86,27 +95,48 @@ struct bstNode *insert(struct bstNode *root, int data) {
   return root;
 }
 
-int minEle(struct bstNode* root){
-	if(root->leftNode==NULL) return root->data;
-	minEle(root->leftNode);
+int minEle(struct bstNode *root) {
+  if (root->leftNode == NULL)
+    return root->data;
+  minEle(root->leftNode);
 }
-int maxEle(struct bstNode* root){
-	if(root->rightNode==NULL) return root->data;
-	maxEle(root->rightNode);
+int maxEle(struct bstNode *root) {
+  if (root->rightNode == NULL)
+    return root->data;
+  maxEle(root->rightNode);
 }
 
-int isBsTree(struct bstNode* root){
-	if(root!=NULL){
-		if(root->leftNode!=NULL){
-			if((root->data) >= (root->leftNode->data)){
-				if(root->rightNode!=NULL){
-					if((root->data) < (root->rightNode->data)){
-						return 1;
-					} else return 0;
-				}
-				return 1;
-			} else return 0;
-		}
-	return (isBsTree(root->leftNode))*(isBsTree(root->rightNode));
-	}
+int isBsTree(struct bstNode *root) {
+  if (root == NULL)
+    return 1;
+  if (root->leftNode != NULL) {
+    if ((root->data) >= (root->leftNode->data)) {
+      if (root->rightNode != NULL) {
+        if ((root->data) < (root->rightNode->data)) {
+          return 1;
+        } else
+          return 0;
+      }
+      return 1;
+    } else
+      return 0;
+  }
+  return (isBsTree(root->leftNode)) * (isBsTree(root->rightNode));
+}
+
+int isbsTree(struct bstNode *root) {
+  return isbsTreeutil(root, MIN_VAL, MAX_VAL);
+}
+
+int isbsTreeutil(struct bstNode *root, int min, int max) {
+  if (root == NULL)
+    return 1;
+  if (root->data >= min && root->data < max) {
+    if (isbsTreeutil(root->leftNode, min, root->data) &&
+        isbsTreeutil(root->rightNode, root->data, max)) {
+      return 1;
+    }
+  } else {
+    return 0;
+  }
 }
